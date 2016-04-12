bf6261674778b6dd88ca2a5f8692c9f1fccd2d9c
Jesse Brizzi <jessebrizzi@users.noreply.github.com>
2016-04-12 01:30:26 -0400

    update

--- internet_check.sh
+++ internet_check.sh
@@ -5,7 +5,7 @@ do
     wget -q --tries=10 --timeout=20 -O - http://google.com > /dev/null
     if ! [[ $? -eq 0 ]]; then
       kdialog --title "OFFLINE" --passivepopup "This popup will disappear in 5 seconds" 5 &
-      sudo /home/jesse/Documents/internet_fix/internet_fix
+      sudo /home/user/path/to/internet_fix
     fi
     sleep 10
 done
